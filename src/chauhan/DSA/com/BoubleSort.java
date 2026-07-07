f59d5f3bc7eed2b15c4b4bb0af45a1c8c0354745
prasadchauhan9090 <prasadchauhan99@gmail.com>
2026-07-07 20:06:27 +0530

    Updated with new package of HahMap  commit

--- src/chauhan/DSA/com/BoubleSort.java
+++ src/chauhan/DSA/com/BoubleSort.java
@@ -4,38 +4,33 @@ public class BoubleSort {
 
     public static void main(String args[])
     {
-        int arr[] = {31,1,5,3,2,12,17};
-        int temp;
-        int n=arr.length-1;
 
-        for(int i=0;i<arr.length;i++) {
-            System.out.print( arr[i] + "  ");
-        }
+        int a[]={9,8,7,6,5,99,3,2,1,4};
 
-        for(int i=0;i<n;i++) {
+       int n=a.length;
+       int temp=0;
 
-            boolean swapped=false;
-            for(int j=0 ;j<n-1-i;j++) {
-
-                if(arr[j]>arr[j+1]) {
-                    temp=arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                    swapped=true;
-                }
-
-            }
-            if(swapped==false) {
-                break;
-            }
-        }
-
-        System.out.println("========================");
-
-        for(int i=0;i<n-1;i++) {
-            System.out.print(arr[i] + "  ");
-        }
-
+       for(int i=0;i<n-1;i++)
+       {
+           boolean swap=false;
+           for(int j=0;j<n-i-1;j++)
+           {
+               if(a[j]>a[j+1]) {
+                   temp = a[j];
+                   a[j] = a[j + 1];
+                   a[j + 1] = temp;
+                   swap = true;
+               }
+           }
+           if(!swap)
+           {
+               break;
+           }
+       }
+       for(int i=0;i<n;i++)
+       {
+           System.out.print(a[i]+" ");
+       }
 
         }
 
